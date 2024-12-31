439d4bf507317341893063be4c4e23e6d316efd8
Bjorn-Ironsidee <109214871+Bjorn-Ironsidee@users.noreply.github.com>
2024-12-31 12:36:13 +1100

    Update app.route.ts
    Implemented routing for each component

--- council-crud-app/src/app/app.routes.ts
+++ council-crud-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CouncilManagementComponent } from './council-management/council-management.component';
+import { RegulationManagementComponent } from './regulation-management/regulation-management.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardComponent }, //Default route
+  { path: 'council-management', component: CouncilManagementComponent },
+  { path: 'regulation-management/:councilId', component: RegulationManagementComponent },
+];
