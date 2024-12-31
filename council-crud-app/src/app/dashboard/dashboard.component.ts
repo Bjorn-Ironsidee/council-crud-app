63ab92fb5083c43feb3137f4c3ba9c738f8a9e87
Bjorn-Ironsidee <109214871+Bjorn-Ironsidee@users.noreply.github.com>
2024-12-31 13:06:42 +1100

    Update dashboard.component.ts
    updateded dashboard.components with logic

--- council-crud-app/src/app/dashboard/dashboard.component.ts
+++ council-crud-app/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  councils = [
+    { id: 1, name: 'Council A' },
+    { id: 2, name: 'Council B' },
+    { id: 3, name: 'Council C' },
+    // Static list of council for now, will be replaced by API call in future
+  ]
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  // Navigate to council management
+  goToCouncilManagement(): void {
+    this.router.navigate(['/council-management']);
+  }
+
+  // Navigate to regulation management for a specific council
+  goToRegulationManagement(councilId: number): void {
+    this.router.navigate(['regulation-management/${councilId}']);
+  }
 
 }
